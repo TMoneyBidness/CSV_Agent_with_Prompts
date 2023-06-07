05da46cfe822fffcba6f34d952bff09de6048b5a
Taylor Erwin <90368987+TMoneyBidness@users.noreply.github.com>
2023-06-07 09:42:56 -0400

    Update CSV_Agent_Deployed.py

--- CSV_Agent_Deployed.py
+++ CSV_Agent_Deployed.py
@@ -89,7 +89,9 @@ def df_agent(df, agent_context, describe_dataset, query):
 st.title("👨‍💻 Query your CSV with an AI Agent using Langchain")
 st.write("Beyond a basic CSV Agent to query your tabular data, this app allows you to provide a prompt to the agent, preview headings, provide task objectives, and contextual information about your data.")
 st.write("Please upload your CSV file below.")
-   
+
+uploaded_file = st.file_uploader("Please upload your CSV file below")
+
 # # Add a password input
 # password = st.text_input("Enter the password to use the default API key", type='password')
 
@@ -98,8 +100,6 @@ st.write("Please upload your CSV file below.")
 #     API_KEY = st.secrets["apikey"]
 # else:
 #     API_KEY = st.text_input("Enter your API key", type='password')
-    
-uploaded_file = st.file_uploader("Please upload your CSV file below")
 
 if uploaded_file is not None:
     if uploaded_file.size == 0:
